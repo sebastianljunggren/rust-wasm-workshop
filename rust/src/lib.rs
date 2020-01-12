0077c6cc8c30b7eb41546b796fff3301eb05e708
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2020-01-12 15:44:49 +0100

    Change function name to count_orbits

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,5 +2,5 @@
 extern crate serde_derive;
 
 pub mod fibonacci;
-pub mod selection_sort;
 pub mod orbits;
+pub mod selection_sort;
--- rust/src/orbits.rs
+++ rust/src/orbits.rs
@@ -2,15 +2,14 @@ use wasm_bindgen::prelude::*;
 
 #[derive(Deserialize)]
 pub struct AstronomicalObject {
-  orbits: Vec<AstronomicalObject>
+    orbits: Vec<AstronomicalObject>,
 }
 
-
 #[wasm_bindgen]
-pub fn orbits(com: &JsValue) -> u32 {
+pub fn count_orbits(com: &JsValue) -> u32 {
     let com: Result<AstronomicalObject, _> = com.into_serde();
     match com {
         Ok(com) => com.orbits.len() as u32,
-        _ => 0
+        _ => 0,
     }
 }
