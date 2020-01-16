3d4c966cebbc5e5db827b3c39bd1fa9fa919df91
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2020-01-16 16:26:48 +0100

    Improve JS implementation of count orbits

--- typescript/src/model/orbits.ts
+++ typescript/src/model/orbits.ts
@@ -2,21 +2,10 @@ export interface AstronomicalObject {
     orbits: AstronomicalObject[]
 }
 
-class AstronomicalObjectImpl implements AstronomicalObject {
-  constructor (public orbits: AstronomicalObjectImpl[]) {}
-
-  countOrbits (depth: number): number {
-    const childOrbits = this.orbits.reduce((sum, ao) => sum + ao.countOrbits(depth + 1), 0)
-    return depth + childOrbits
-  }
-}
-
-function fromObjectToClass (ao: AstronomicalObject): AstronomicalObjectImpl {
-  const orbits = ao.orbits.map(fromObjectToClass)
-  return new AstronomicalObjectImpl(orbits)
+function countOrbits (astronomicalObject: AstronomicalObject, depth: number): number {
+  return depth + astronomicalObject.orbits.reduce((sum, ao) => sum + countOrbits(ao, depth + 1), 0)
 }
 
 export function jsCountOrbits (com: AstronomicalObject): number {
-  const depth = 0
-  return fromObjectToClass(com).countOrbits(depth)
+  return countOrbits(com, 0)
 }
